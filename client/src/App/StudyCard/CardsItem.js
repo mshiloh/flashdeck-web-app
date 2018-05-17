6652579b9c3fc5615c2e48ef1a90c300c0d18f13
mshiloh <mshiloh.code@gmail.com>
2018-05-16 21:13:41 -0600

    re-did style

--- client/src/App/StudyCard/CardsItem.js
+++ client/src/App/StudyCard/CardsItem.js
@@ -4,13 +4,22 @@ export default class CardsItem extends Component {
         super(props);
         this.state = {
             showAnswer: false,
+            showQuestion: true
         };
         this.revealAnswer = this.revealAnswer.bind(this);
+        this.backToQuestion = this.backToQuestion.bind(this);
     }
 
     revealAnswer() {
         this.setState({
             showAnswer: true,
+            showQuestion: false,
+        });
+    }
+    backToQuestion() {
+        this.setState({
+            showAnswer: false,
+            showQuestion: true,
         });
     }
 
@@ -21,15 +30,25 @@ export default class CardsItem extends Component {
 
             <div className="wrapperQA">
                 <div className="containerQ">
-                    <p>{question}</p>
+                    {this.state.showQuestion ?
+                        <p>{question}</p> : null
+                    }
+                    {this.state.showAnswer ?
+                        <p>{answer}</p> : null
+                    }
+
                 </div>
 
-                {this.state.showAnswer ?
-                    <p className="correctAnswer">【 {answer} 】</p> : null
-                }
                 <div className="checkWrapper">
-                    <button onClick={this.revealAnswer} className="checkButt">Check</button>
+                    {this.state.showQuestion ?
+                        <button onClick={this.revealAnswer} className="checkButt">Check </button> : null
+                    }
+
+                    {this.state.showAnswer ?
+                        <button onClick={this.backToQuestion} className="checkButt">Back to Question</button> : null
+                    }
                 </div>
+
                 <form className="answerForm">
                     <input className="answerInput" type="text" placeholder="Enter answer..." />
                 </form>
@@ -37,3 +56,45 @@ export default class CardsItem extends Component {
         )
     }
 }
+
+
+
+// import React, { Component } from 'react'
+// export default class CardsItem extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             showAnswer: false,
+//         };
+//         this.revealAnswer = this.revealAnswer.bind(this);
+//     }
+
+//     revealAnswer() {
+//         this.setState({
+//             showAnswer: true,
+//         });
+//     }
+
+//     render() {
+//         const { deckId } = this.props.deckId._id;
+//         const { question, answer, _id, endpoint } = this.props;
+//         return (
+
+//             <div className="wrapperQA">
+//                 <div className="containerQ">
+//                     <p>{question}</p>
+//                 </div>
+
+//                 {this.state.showAnswer ?
+//                     <p className="correctAnswer">【 {answer} 】</p> : null
+//                 }
+//                 <div className="checkWrapper">
+//                     <button onClick={this.revealAnswer} className="checkButt">Check</button>
+//                 </div>
+//                 <form className="answerForm">
+//                     <input className="answerInput" type="text" placeholder="Enter answer..." />
+//                 </form>
+//             </div>
+//         )
+//     }
+// }
